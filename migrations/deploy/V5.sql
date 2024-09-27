88bdab9409483183a901f4b4ab90a64e8a36fab9
MickaelCarlucci <frewmike17@gmail.com>
2024-09-27 13:27:35 +0200

    fix name column

--- migrations/deploy/V5.sql
+++ migrations/deploy/V5.sql
@@ -7,12 +7,12 @@ CREATE table "appointment" (
      "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
      "subject" TEXT NOT NULL,
      "date" TEXT NOT NULL,
-     "link" TEXT NOT NULL,
+     "linkMeeting" TEXT NOT NULL,
      "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
      "updated_at" TIMESTAMPTZ
 );
 
-INSERT INTO "appointment" ("subject", "date", "link")
+INSERT INTO "appointment" ("subject", "date", "linkMeeting")
 VALUES
 ('A définir', 'A définir', 'A définir');
 
