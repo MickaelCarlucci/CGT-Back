12fdbee976b63004c09ddad5797a9197cb3eac8c
MickaelCarlucci <frewmike17@gmail.com>
2025-01-29 15:49:33 +0100

    add rights after upload

--- app/controllers/pdf.controller.js
+++ app/controllers/pdf.controller.js
@@ -13,23 +13,24 @@ export default {
             return response.status(400).json({error: "aucun fichier téléchargé"})
         }
         const title = request.file.originalname;
-        //construit automatiquement l'url pour la bdd
         const pdf_url = `/uploads/${request.file.filename}`;
-
-
-        
-
-        const newPdf = await pdfDatamapper.create(
-            title,
-            pdf_url,
-            section_id,
-            center_id
-        )
-        if (!newPdf) {
-            return response.status(500).json({error: "Le fichier n'a pas pu être uploadé dans la base de donnée"})
+    
+        // Changer les permissions après l'upload
+        try {
+          fs.chmodSync(`uploads/${request.file.filename}`, 0o755);
+          console.log(`Permissions modifiées pour : ${request.file.filename}`);
+        } catch (error) {
+          console.error("Erreur lors de la modification des permissions :", error);
         }
-        return response.status(201).json({message: "Fichier téléchargé avec succès", fichier: newPdf})        
-    },
+    
+        const newPdf = await pdfDatamapper.create(title, pdf_url, section_id, center_id);
+        if (!newPdf) {
+          return response.status(500).json({ error: "Le fichier n'a pas pu être ajouté à la base de données" });
+        }
+    
+        return response.status(201).json({ message: "Fichier téléchargé avec succès", fichier: newPdf });
+      },
+    
 
     download: async (request, response) => {
         const {filename} = request.params;
