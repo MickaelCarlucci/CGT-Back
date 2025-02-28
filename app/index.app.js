dd1d04b424faf33acfa16643d356d84c45cd61b2
MickaelCarlucci <frewmike17@gmail.com>
2025-02-28 15:26:43 +0100

    change CORS rules

--- app/index.app.js
+++ app/index.app.js
@@ -4,7 +4,11 @@ import { fileURLToPath } from "url";
 import path from "path";
 import router from "./routers/index.router.js";
 
-const allowedOrigins = ["https://cgt-tp.fr"];
+const allowedOrigins = [
+  "https://cgt-tp.fr",
+  "https://app-cgt-teleperformance.firebaseapp.com",
+  "https://app-cgt-teleperformance.web.app",
+];
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -17,7 +21,7 @@ if (!fs.existsSync(uploadsPath)) {
   console.error("❌ ERREUR : Le dossier uploads n'existe pas !");
 }
 
-/*app.use(
+app.use(
   cors({
     origin: function (origin, callback) {
       if (!origin || allowedOrigins.includes(origin)) {
@@ -26,16 +30,31 @@ if (!fs.existsSync(uploadsPath)) {
         callback(new Error("Non autorisé par les règles CORS"));
       }
     },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // Autorise les cookies et l'auth Firebase
   })
-);*/
+);
 
-app.use(
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://app-cgt-teleperformance.firebaseapp.com, https://app-cgt-teleperformance.web.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // Si Firebase Auth utilise des cookies
+  next();
+});
+
+/*app.use(
   cors({
     origin: "*",
   })
-);
+);*/
 
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
